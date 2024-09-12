87c90b2ae29f348927d4b8649b094cd5386ada23
Vaishnav-sabari-girish <88036970+TheGodOfCoding2004@users.noreply.github.com>
2024-09-12 18:46:53 +0530

    Update linear_convolution.m

--- EXPERIMENT 2/linear_convolution.m
+++ EXPERIMENT 2/linear_convolution.m
@@ -1,6 +1,5 @@
 %This program performs the Linear Convolution operation on 2 signals
 
-
 clc;
 clear all;
 close all;
@@ -8,8 +7,44 @@ close all;
 x = input("Enter the sequence x(n) : ")
 h = input("Enter the sequence h(n) : ")
 
-y = conv(x , h)       %Linear Convolution
+y = conv(x , h)
+figure(1)
+stem(x)
+title("Input signal x(n)")
+xlabel("Time Index")
+ylabel("Amplitude")
 
+figure(2)
+stem(h)
+title("Input Signal h(n)")
+xlabel("Time Index")
+ylabel("Amplitude")
+
+figure(3)
+stem(y)
+title("Linear Convolution")
+xlabel("Time Index")
+ylabel("Amplitude")clc;
+clear all;
+close all;
+
+x = input("Enter the sequence x(n) : ")
+h = input("Enter the sequence h(n) : ")
+
+y = conv(x , h)
+figure(1)
+stem(x)
+title("Input signal x(n)")
+xlabel("Time Index")
+ylabel("Amplitude")
+
+figure(2)
+stem(h)
+title("Input Signal h(n)")
+xlabel("Time Index")
+ylabel("Amplitude")
+
+figure(3)
 stem(y)
 title("Linear Convolution")
 xlabel("Time Index")
